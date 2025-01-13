452a99da244ed327a183a9ea62361b33fe3bbfd5
iamevaristus <evaristusadimonyemma@serchservice.com>
2025-01-13 11:22:05 +0100

    Resolving const error with web environment variables

--- main/lib/backend/keys.dart
+++ main/lib/backend/keys.dart
@@ -4,27 +4,27 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class Keys {
   /// Serch api key
-  static final String _apiKey = "API_KEY";
+  static const String _apiKey = "API_KEY";
   static final String apiKey = PlatformEngine.instance.isWeb
-      ? String.fromEnvironment(_apiKey)
+      ? const String.fromEnvironment(_apiKey)
       : dotenv.env[_apiKey] ?? "";
 
   /// Serch secret key
-  static final String _secretKey = "SECRET_KEY";
+  static const String _secretKey = "SECRET_KEY";
   static final String secretKey = PlatformEngine.instance.isWeb
-      ? String.fromEnvironment(_secretKey)
+      ? const String.fromEnvironment(_secretKey)
       : dotenv.env[_secretKey] ?? "";
 
   /// Serch signature
-  static final String _signature = "SIGNATURE";
+  static const String _signature = "SIGNATURE";
   static final String signature = PlatformEngine.instance.isWeb
-      ? String.fromEnvironment(_signature)
+      ? const String.fromEnvironment(_signature)
       : dotenv.env[_signature] ?? "";
 
   /// Google map api key
   static String get googleMapApiKey {
     if(PlatformEngine.instance.isWeb) {
-    return dotenv.env["GOOGLE_MAPS_API_KEY_WEB"] ?? "";
+      return dotenv.env["GOOGLE_MAPS_API_KEY_WEB"] ?? "";
     } else if(PlatformEngine.instance.isAndroid) {
       return dotenv.env["GOOGLE_MAPS_API_KEY_ANDROID"] ?? "";
     } else if(PlatformEngine.instance.isIOS) {
